79607af9856f444e40c06512bf4f617256f4bc4a
Daniel-1017 <frimudaniel17@gmail.com>
2025-01-05 15:51:03 +0100

    using config to get the db url & providing the nestjs config globally

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor() {
+  constructor(config: ConfigService) {
     super({
       datasources: {
         db: {
-          url: 'mongodb+srv://frimudaniel:1234@cluster0.bhqi5.mongodb.net/nestjs',
+          url: config.get('DATABASE_URL'),
         },
       },
     });
