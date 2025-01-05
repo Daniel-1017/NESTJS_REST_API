4a6fbc0725e62674da4fb82589b242f1c471732d
Daniel-1017 <frimudaniel17@gmail.com>
2025-01-05 15:43:21 +0100

    signin login & passing dto to auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -42,7 +42,26 @@ export class AuthService {
     }
   }
 
-  signin() {
-    return 'I am signin';
+  async signin(dto: AuthDto) {
+    // find the user by email
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    // if user does not exist throw exception
+    if (!user)
+      throw new ForbiddenException(`User with email ${dto.email} not found`);
+
+    // compare password
+    const pwMatches = await argon.verify(user.hash, dto.password);
+
+    // if password incorrect throw exception
+    if (!pwMatches) throw new ForbiddenException('Password is incorrect');
+
+    // send back the user
+    delete user.hash;
+    return user;
   }
 }
