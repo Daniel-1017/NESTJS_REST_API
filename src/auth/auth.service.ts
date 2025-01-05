6385e55216f941170ee6fa72d21d98c2fea2ae7e
Daniel-1017 <frimudaniel17@gmail.com>
2025-01-05 16:13:57 +0100

    return object with access token instead of access token string

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -63,15 +63,22 @@ export class AuthService {
     return this.signToken(user.id, user.email);
   }
 
-  signToken(userId: string, email: string): Promise<string> {
+  async signToken(
+    userId: string,
+    email: string,
+  ): Promise<{ access_token: string }> {
     const payload = {
       sub: userId,
       email,
     };
 
-    return this.jwt.signAsync(payload, {
+    const token = await this.jwt.signAsync(payload, {
       expiresIn: '15m',
       secret: this.config.get('JWT_SECRET'),
     });
+
+    return {
+      access_token: token,
+    };
   }
 }
